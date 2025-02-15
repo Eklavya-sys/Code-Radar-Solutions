db454f366c1ef070a49117580c56c1203eca1e41
Eklavya-sys <eklavyagautam20@gmail.com>
2025-02-15 17:04:20 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    char grade[5];
-    char description[6];
+    char grade[50];
+    char description[60];
     printf("");
     scanf("%c %c",&grade,&description);
     if(grade=='A'){
