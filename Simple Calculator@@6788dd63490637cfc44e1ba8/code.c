6e29057d97605414a74feba25ca780ba0a4c6b80
Eklavya-sys <eklavyagautam20@gmail.com>
2025-02-15 16:55:28 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,32 +1,29 @@
-#include <stdio.h>
-
-int main() {
-    int a, b;
+#include<stdio.h>
+int main(){
+    int a,b;
     char operator;
-
-    // Read input
-    scanf("%d %d %c", &a, &b, &operator);
-
-    // Check the operator and perform the corresponding operation
-    if (operator == '+') {
-        printf("%d + %d = %d\n", a, b, a + b);
-    } 
-    else if (operator == '-') {
-        printf("%d - %d = %d\n", a, b, a - b);
-    } 
-    else if (operator == '*') {
-        printf("%d * %d = %d\n", a, b, a * b);
-    } 
-    else if (operator == '/') {
-        if (b != 0) {
-            printf("%d / %d = %.2f\n", a, b, (float)a / b);
-        } else {
-            printf("Error: Division by zero is not allowed.\n");
-        }
-    } 
-    else {
-        printf("Error: Unsupported operator.\n");
+    printf("");
+    scanf("%d %d %c",&a,&b,&operator);
+    if(operato=='+'){
+        printf("%d",a,b,a+b);
     }
-
+    else if(operator=='-'){
+        printf("%d",a,b,a-b);
+    }
+    else if(operator=='*'){
+        printf("%d",a,b,a*b);
+    }
+    else if(operator=='/'){
+        if(b!=0){
+            printf("%d",a,b,a/b);
+        else{
+            printf("Error");
+        }
+        }
+    }
+    else{
+        printf("Error");
+    }
     return 0;
-}
+        }
+
