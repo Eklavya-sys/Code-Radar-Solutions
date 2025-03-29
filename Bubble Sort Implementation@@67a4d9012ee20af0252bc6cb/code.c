567d74bae9ce34199f9d23d944f73a2fdabe6db1
Eklavya-sys <eklavyagautam20@gmail.com>
2025-03-29 16:09:34 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,38 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+
+void bubble_sort(int arr[], int n) {
     int i, j, temp;
-    for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
+    int swapped;
+
+    
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+
+        
+        for (j = 0; j < n - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+                
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                swapped = 1;
             }
+        }
+
+        
+        if (swapped == 0) {
+            break;
         }
     }
 }
 
-void printArray(int arr[], int size) {
-    int i;
-    for(i = 0; i < size; i++) {
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
+
 int main() {
-    int arr[5] = {64, 34, 25, 12, 22};  
-    int i, n;
-    n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: \n");
-    printArray(arr, n);
+    printf("Unsorted array: \n");
+    print_array(arr, n);
 
-    bubbleSort(arr, n);
+    
+    bubble_sort(arr, n);
 
-    printf("Sorted array: ");
-    printArray(arr, n);
+    printf("Sorted array: \n");
+    print_array(arr, n);
 
     return 0;
 }
